a54b40d62ccdbda64f8c1a9bea2f73409d2b75ac
eduardo <eduardomlobo@yahoo.com.br>
2026-02-06 17:23:33 -0300

    MLflow Part 4

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,15 @@
 import logging
 from dotenv import load_dotenv
+import dagshub
 
 load_dotenv()
+
+# Initialize DagsHub with credentials
+dagshub.init(
+	repo_owner='eduardolmlobo',
+	repo_name='mlops_project'
+)
+
 
 # Configure the logging strategy
 logging.basicConfig(
